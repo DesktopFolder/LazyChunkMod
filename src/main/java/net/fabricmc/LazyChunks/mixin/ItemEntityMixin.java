c9cae213ef60fac0bb92715edbcbc9ba597422d7
DesktopFolder <desktopdoesminecraft@gmail.com>
2022-02-20 00:40:57 -0500

    Fix mixins. Fixins. Mix.

--- src/main/java/net/fabricmc/LazyChunks/mixin/ItemEntityMixin.java
+++ src/main/java/net/fabricmc/LazyChunks/mixin/ItemEntityMixin.java
@@ -1,22 +1,19 @@
 package net.fabricmc.LazyChunks.mixin;
 
 import net.fabricmc.LazyChunks.LazyChunkMod;
-import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.world.entity.item.ItemEntity;
-
-// Sponge Includes (Mixin infrastructure)
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChunkHolder.class)
-public class ChunkHolderMixin {
-//	@Inject(at = @At("HEAD"), method = "tick", cancellable = true)
-//	private void tick(CallbackInfo info) {
-//		if (LazyChunkMod.itickFrozen)
-//		{
-//			info.cancel();
-//		}
-//	}
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+	@Inject(at = @At("HEAD"), method = "tick", cancellable = true)
+	private void tick(CallbackInfo info) {
+		if (LazyChunkMod.itickFrozen)
+		{
+			info.cancel();
+		}
+	}
 }
