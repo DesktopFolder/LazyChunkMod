0fb724a86b8167b0ada7cf7f85faabdc7e36ac78
DesktopFolder <desktopdoesminecraft@gmail.com>
2022-02-26 14:32:13 -0500

    v1.0.2: Add status queries.

--- src/main/java/net/fabricmc/LazyChunks/LazyChunkMod.java
+++ src/main/java/net/fabricmc/LazyChunks/LazyChunkMod.java
@@ -14,37 +14,47 @@ import org.slf4j.LoggerFactory;
 import static net.minecraft.commands.Commands.literal;
 
 public class LazyChunkMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("lazychunks");
-	public static boolean itickFrozen = false;
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("lazychunks");
+    public static boolean itickFrozen = false;
 
-	public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher, Commands.CommandSelection arg) {
-		dispatcher.register(literal("itick")
-				.then(literal("freeze").executes(LazyChunkMod::itickFreeze))
-		);
-	}
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher, Commands.CommandSelection arg) {
+        dispatcher.register(literal("itick")
+                .then(literal("freeze").executes(LazyChunkMod::itickFreeze))
+                .then(literal("status").executes(LazyChunkMod::itickStatus))
+        );
+    }
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		// GERONIMOOOOOOOOOOOOOOOOOOOOOO
-		LOGGER.info("Initializing Lazy Chunk Mod. (By doing nothing. It's lazy, after all.)");
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        // GERONIMOOOOOOOOOOOOOOOOOOOOOO
+        LOGGER.info("Initializing Lazy Chunk Mod. (By doing nothing. It's lazy, after all.)");
+    }
 
-	private static int itickFreeze(CommandContext<CommandSourceStack> c) {
-		itickFrozen = !itickFrozen;
-		var source = c.getSource();
-		if (source == null)
-		{
-			LOGGER.info("Invalid source. Your game will now crash. Try harder next time.");
-		}
-		LOGGER.info(source.getDisplayName().getString());
-		var str = itickFrozen ? "Game is frozen. You may now break the bedrock." : "Game is not frozen. You should turn off the heat.";
-		source.sendSuccess(new TextComponent(str).withStyle(Style.EMPTY.withItalic(true).withColor(ChatFormatting.GRAY)), false);
-		return 0 /* Toto - "This is the amount of 'something' that gets executed." Read: Not a way to subtly mess with people. For shame.*/;
-	}
+    private static void formatQuietResponse(CommandSourceStack source, String response) {
+        if (source == null) {
+            LOGGER.info("Invalid source for 'formatQuietResponse'. Sounds like a you problem, but fine, we won't crash... this time.");
+            return;
+        }
+        LOGGER.info(source.getDisplayName().getString());
+        source.sendSuccess(new TextComponent(response).withStyle(Style.EMPTY.withItalic(true).withColor(ChatFormatting.GRAY)), false);
+    }
+
+    private static int itickFreeze(CommandContext<CommandSourceStack> c) {
+        itickFrozen = !itickFrozen;
+        formatQuietResponse(c.getSource(), itickFrozen ? "Items are frozen. You may now break the bedrock." :
+                "Items are not frozen. You should turn off the heat - that wasn't going to work anyways.");
+        return 0 /* Toto: "This is the amount of 'something' that gets executed." Read: Not a way to subtly mess with people. For shame.*/;
+    }
+
+    private static int itickStatus(CommandContext<CommandSourceStack> c) {
+        formatQuietResponse(c.getSource(), itickFrozen ? "Items are frozen... you couldn't tell? Seriously?" :
+                "Items are not frozen. No, really. Pick one up and see.");
+        return 0 /* Toto: "This is the amount of 'something' that gets executed." Read: Not a way to subtly mess with people. For shame.*/;
+    }
 }
